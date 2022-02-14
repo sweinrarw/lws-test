b384d9cf6c2f19f6ed488efc415a86fdd03b4862
Seth Weiner <sweiner@fonteva.com>
2022-02-14 13:11:25 -0500

    first commit

---
+++ force-app/test/jest-mocks/lightning/messageService.js
@@ -0,0 +1,23 @@
+/**
+ * For the original lightning/messageService (LMS) stub that comes by default with
+ * @salesforce/sfdx-lwc-jest, see:
+ * https://github.com/salesforce/sfdx-lwc-jest/blob/master/src/lightning-stubs/messageService/messageService.js
+ */
+export const APPLICATION_SCOPE = Symbol('APPLICATION_SCOPE');
+export const createMessageChannel = jest.fn();
+export const createMessageContext = jest.fn();
+export const MessageContext = jest.fn();
+export const releaseMessageContext = jest.fn();
+export const unsubscribe = jest.fn();
+
+// Assigns a handler for each channel subscribed, so that multiple channels can be subscribed to
+// within the same test execution context
+const handlers = {};
+export const publish = jest.fn((messageContext, messageChannel, message) => {
+  handlers[messageChannel]?.(message);
+});
+export const subscribe = jest.fn(
+  (messageContext, messageChannel, messageHandler) => {
+    handlers[messageChannel] = messageHandler;
+  }
+);
